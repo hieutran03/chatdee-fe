65cc8111e871ee1fa3013af0e4c1c0f0d3e5dd35
hieutran03 <hieumdg@gmail.com>
2025-10-23 14:09:51 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  server: { port: 8080 },
+  server: { port: 8000 },
   plugins: [react()],
   resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
 });
 // https://vite.dev/config/
+0;
